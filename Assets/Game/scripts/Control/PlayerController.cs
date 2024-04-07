4629467b8cc50880e635700494cd3cf51c23732f
Shwetank Singh <ssingh.21.2019@gmail.com>
2024-04-07 10:02:57 +0530

    safe upload25
    load scene null reference

--- Assets/Game/scripts/Control/PlayerController.cs
+++ Assets/Game/scripts/Control/PlayerController.cs
@@ -13,13 +13,7 @@ namespace RPG.Control
         Health health;
         // this script is responsible for controlling all the things like input controls 
 
-        enum CursorType
-        {
-            None,
-            Movement,
-            Combat,
-            UI
-        }
+       
 
         [System.Serializable]
         struct CursorMapping
@@ -75,7 +69,7 @@ namespace RPG.Control
                 {
                     if (raycastable.HandleRaycast(this))
                     {
-                        SetCursor(CursorType.Combat);
+                        SetCursor(raycastable.GetCursorType());
                         return true;
                     }
                 }
