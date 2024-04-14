10456bbf38ac7cbaaebe55ba373e7050dbd12588
Shwetank Singh <ssingh.21.2019@gmail.com>
2024-04-14 16:08:40 +0530

    safe upload33

---
+++ Assets/Game/scripts/Attributes/HealthBar.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Attribute
+{
+    public class HealthBar : MonoBehaviour
+    {
+        
+        void Update()
+        {
+
+        }
+    }
+}
